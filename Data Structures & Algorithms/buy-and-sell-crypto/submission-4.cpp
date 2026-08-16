c1a628de57b99e350a807ee1f66a560f890a9101
jai-kardam <jaikardam8@gmail.com>
2026-08-16 22:13:56 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     int buy=prices[0];
+     int profit=0;
+     for(int i=0;i<prices.size();i++){
+      while(i<prices.size() &&buy<prices[i]){
+        int calc=prices[i]-buy;
+        profit=max(profit,calc);
+        i++;
+      }
+      if(i<prices.size())
+      buy=prices[i];
+
+     } 
+     return profit;  
+    }
+};
